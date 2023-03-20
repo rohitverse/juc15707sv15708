d9a923a8534a586acfbe66fcb5415742abc7b126
rohitverse <rohitkr23072001@gmail.com>
2023-03-20 15:48:01 +0530

    Add Constant Program

---
+++ 006-Constants.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+contract Constant {
+    uint public constant MY_FAV_NUM = 5;
+    address public constant MY_ADDRESS =
+        0x0000000000000000000000000000000000000001;
+}
