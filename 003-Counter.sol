2e1211d943cfb728dcb10fd3ae33d153c12083e5
rohitverse <rohitkr23072001@gmail.com>
2023-03-20 12:45:30 +0530

    Add Counter Program

---
+++ 003-Counter.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier : MIT
+pragma solidity ^0.8.8;
+
+contract Counter {
+    uint256 public count;
+
+    function getCount() public view returns (uint256) {
+        return count;
+    }
+
+    function incCount() public {
+        count += 1;
+    }
+
+    function decCount() public {
+        count -= 1;
+    }
+}
