41c094dba3b275b9bec878bdf10491a57a268642
rohitverse <rohitkr23072001@gmail.com>
2023-03-20 14:41:21 +0530

    Add Primitive DataType Program

---
+++ 004-PrimitiveDataTypes.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+contract PrimitiveDataTypes {
+    bool public isTrue = true; //default value false
+    int256 public favNumber = -7; //default value 0
+    uint256 public number = 7; //default value 0
+    address public accountAddress = 0x0000000000000000000000000000000000000001; //default value 0x0000000000000000000000000000000000000000
+}
